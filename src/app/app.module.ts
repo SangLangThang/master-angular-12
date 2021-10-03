3c0e7d4707c1ec4529f07054de40d2f92ef39e7c
DESKTOP-GT7JI5F\sangn <sang52ck@gmail.com>
2021-10-02 23:23:07 -0700

    finish section 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './section1/server/server.component';
 import { WarningAlertComponent } from './section2/warning-alert/warning-alert.component';
 import { SuccessAlertComponent } from './section2/success-alert/success-alert.component';
 import { Section2Component } from './section2/section2.component';
+import { ServersComponent } from './section1/servers/servers.component';
 
 @NgModule({
   declarations: [
--- src/app/section2/section2.component.ts
+++ src/app/section2/section2.component.ts
@@ -3,5 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-section2',
   templateUrl: './section2.component.html',
+  styles: ['.white { color: white; }']
 })
-export class Section2Component {}
+export class Section2Component {
+  username:string=''
+  showDetails:boolean=false
+  btnClicks=[]
+  onClick(){
+    this.showDetails=!this.showDetails
+    this.btnClicks.push(new Date())
+  }
+}
